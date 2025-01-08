bcb78518bd5f8b010d22b0d559190e72fac2c056
beatrizivano <beatrizivano56@gmail.com>
2025-01-08 17:31:17 -0300

    protótipo Armas e Mapa

--- Mapa+Combate/weapon.py
+++ Mapa+Combate/weapon.py
@@ -12,3 +12,40 @@ iron_sword = Weapon(name="Iron Sword",atk=5)
 short_bow = Weapon(name="Short Bow", atk=4)
 
 fists = Weapon(name="Fists", atk=2)
+
+class Arma:
+    def __init__(self,
+                 name: str,
+                 type: str,
+                 damage: int,
+                 value: int
+                 ) -> None:
+        self.name = name
+        self.type = type
+        self.damage = damage
+        self.value = value
+
+espada = Arma(name="Espada",
+              type="lâmina",
+              damage=5,
+              value=10)
+
+adaga = Arma(name="Adaga",
+             type="lâmina",
+             damage=3,
+             value=5)
+
+arco = Arma(name="Arco",
+            type="disparo",
+            damage=4,
+            value=8)
+
+grimório = Arma(name="Grimório",
+                type="mágico",
+                damage=4,
+                value=9)
+
+punhos = Arma(name="Punhos",
+              type="impacto",
+              damage=2,
+              value=0)
--- main.py
+++ main.py
@@ -1,3 +1,16 @@
-print("teste")
+from map import Map
+import os
 
-print ("teste teste")
+os.system("")
+
+
+def run() -> None:
+    while True:
+        game_map.display_map()
+        input("> ")
+
+
+if __name__ == "__main__":
+    map_w, map_h = 30, 15
+    game_map = Map(map_w, map_h)
+    run()
