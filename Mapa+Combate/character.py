73b84881bb59807955f2c9917ce3002e2a6bc31b
Lunebyte <lunebyte@gmail.com>
2025-01-08 17:20:56 -0300

    Testes de combate

---
+++ Mapa+Combate/character.py
@@ -0,0 +1,22 @@
+from weapon import * 
+
+class Character:
+    def __init__(self, name: str, hp: int, atk: int):
+        self.name = name
+        self.hp = hp
+        self.hp_max = hp
+        self.atk = atk
+        self.weapon = iron_sword
+
+    def attack(self, target) -> None:
+        target.hp -= self.atk + self.weapon.atk
+        target.hp = max(target.hp, 0)
+        print(f"{self.name} deu {self.atk + self.weapon.atk} de dano ao {target.name}")
+
+class Main_Character(Character):
+    def __init__(self, name: str, hp:int) -> None:
+        super().__init_(name=name, hp = hp)
+
+class Enemy(Character):
+    def __init__(self, name: str, hp:int) -> None:
+        super().__init_(name=name, hp = hp)
