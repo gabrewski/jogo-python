17a21ced3f6f0f7496620395266a279209172177
Lunebyte <lunebyte@gmail.com>
2025-01-11 16:10:29 -0300

    Sistema de Terreno (Protótipo)

---
+++ Mapa+Combate/Mapa (Protótipo)/tile.py
@@ -0,0 +1,24 @@
+ANSI_RESET = "\033[0m"
+ANSI_YELLOW = "\033[33m"
+ANSI_GREEN = "\033[32m"
+ANSI_BLUE = "\033[34m"
+ANSI_RED = "\033[31m"
+ANSI_WHITE = "\033[97m"
+ANSI_MAGENTA = "\033[35m"
+ANSI_CYAN = "\033[36m"
+
+class Tile:
+    def __init__(self, symbol: str, color:str = ANSI_RESET, colored: bool = True):
+        if colored:
+            self.symbol= f"{color}{symbol}{ANSI_RESET}"
+        else:
+            self.symbol=symbol
+
+planice = Tile("_", ANSI_GREEN)
+floresta = Tile("♣", ANSI_GREEN)
+montanha = Tile("▲", ANSI_WHITE)
+agua = Tile("≈", ANSI_CYAN)
+areia = Tile("~", ANSI_YELLOW)
+tundra = Tile ("‗", ANSI_WHITE)
+pantano = Tile("#", ANSI_GREEN)
+magma= Tile("≋", ANSI_RED)
