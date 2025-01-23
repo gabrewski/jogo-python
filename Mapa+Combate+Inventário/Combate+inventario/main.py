058baf7858cdc54d5f62bd253a769cfa400f629c
beatrizivano <beatrizivano56@gmail.com>
2025-01-23 15:04:11 -0300

    exclusão de arquivos que não iam ser utilizados

--- Mapa+Combate+Inventário/Combate+inventario/main.py
+++
@@ -1,9 +0,0 @@
-from character import *
-from item import *
-from combat_interface import *
-from inventario import *
-
-player = Character (name="Dargia", hp=100, atk=20, crit_chance=0.4, crit_damage=2.0)
-enemy = Character (name="Goblin", hp=100, atk=20, crit_chance=0.4, crit_damage=2.0)
-
-
