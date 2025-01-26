141d2f77ab5489bc3db30424a55e55e7a8a0dfbc
Lunebyte <lunebyte@gmail.com>
2025-01-26 17:57:34 -0300

    Fix Movimentação no Map Loop

--- src/Mapa/main.py
+++ src/Mapa/main.py
@@ -15,15 +15,30 @@ def map_loop(player, stage_choice:int, size:tuple[int, int], interface, stdscr):
     maps = (ForestMap(*size), DesertMap(*size), SnowMap(*size), SwampMap(*size), FireMap(*size))
     game_map = maps[stage_choice-1]
     encounter_chance = 0
+    
+    # Função que desenha a interface do modo de exploração 
+    while True: # Loop principal do modo de exploração
+        interface(game_map.display_map()) # Desenha o mapa
+        key = stdscr.getch() # Pega a tecla pressionada
+        key_char = chr(key).lower() if key != -1 else '' # Converte a tecla para string
+        
+        if key_char == 'm': # Voltar para o mapa principal 
+            break 
+        
+        
+        # Movimentação do jogador
+        if key_char in ('w', 'a', 's', 'd'): 
+            
+            new_pos = player.pos.copy()
+            if key_char == 'w': new_pos[1] -= 1
+            elif key_char == 's': new_pos[1] += 1
+            elif key_char == 'a': new_pos[0] -= 1
+            elif key_char == 'd': new_pos[0] += 1
+            
+            if 0 <= new_pos[0] < game_map.largura and 0 <= new_pos[1] < game_map.altura:
+                player.pos = new_pos
 
-    while True:
-        interface(game_map.display_map())
-        key = stdscr.getch()
-        key_char = chr(key).lower() if key != -1 else ''
-        # Voltar para o mapa principal
-        if key_char == 'm':
-            return 
-
+        
         game_map.update_map(player.pos, player.marker)
 
 '''# Função que roda o loop principal do modo de exploração de mapa aleatório 
