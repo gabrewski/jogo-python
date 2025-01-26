7998e980b27ece247bdcf4f3d2ce58f6e03a9f24
Lunebyte <lunebyte@gmail.com>
2025-01-26 15:18:23 -0300

    Fix Mapa

--- src/Mapa/main.py
+++ src/Mapa/main.py
@@ -8,30 +8,40 @@ from Mapa.encounter import roll_encounter
 from Combat.combat_test import combat
 import msvcrt
 
-'''def map_loop(player, stage):
-    maps = (ForestMap(166, 46), DesertMap(166, 46), 
-            SnowMap(166, 46), SwampMap(166, 46), FireMap(166, 46))
-    game_map = maps[stage-1]
-    
-    while True:
-        
-        game_map.display_map()
-        
-        
-        key = player.get_movement_input(game_map.largura, game_map.altura)
-        
-        if key.lower() == 'q':  
-            return
-        elif key in ("w", "a", "s", "d"):
-            
-            enemy = roll_encounter(stage, 0)
-            if enemy:
-                if combat(player, enemy):
-                    continue
-                else:
-                    return'''
+def map_loop(player, stage_choice:int, interface):
+    maps = (ForestMap(166, 46), DesertMap(166, 46), SnowMap(166, 46), SwampMap(166, 46), FireMap(166, 46))
+    game_map = maps[stage_choice-1]
+    encounter_chance = 0
 
-def map_loop(player, stage_choice:int):
+    while True:
+        interface(game_map.display_map())
+
+        print("\nPressione [W] para mover para cima, [S] para baixo, [A] para esquerda e [D] para direita.")
+        print("\nPressione [1] para voltar ao mapa principal")
+
+        key = player.get_movement_input(game_map.largura, game_map.altura)
+
+        # if key in ("w", "W", "s", "S", "a", "A", "d", "D"):
+        #         enemy = roll_encounter(int(stage_choice), encounter_chance)
+        #         if enemy:
+        #             os.system('cls')
+        #             encounter_chance = 0
+
+        #             if combat(player, enemy):
+        #                 continue
+        #             else:
+        #                 return
+
+        #         else:
+        #             encounter_chance += 2
+
+        # elif key == '1':
+        #      return
+
+        game_map.update_map(player.pos, player.marker)
+
+
+'''def map_loop(player, stage_choice:int, stdscr):
     maps = (ForestMap(166, 46), DesertMap(166, 46), SnowMap(166, 46), SwampMap(166, 46), FireMap(166, 46))
     game_map = maps[stage_choice-1]
     encounter_chance = 0
@@ -61,6 +71,6 @@ def map_loop(player, stage_choice:int):
         # elif key == '1':
         #      return
 
-        game_map.update_map(player.pos, player.marker)
+        game_map.update_map(player.pos, player.marker)'''
 
     
